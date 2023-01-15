793c0dcdcf89da8cc3c487c3655f691f40336f86
NanamiNakano <thynanami@foxmail.com>
2023-01-15 20:55:57 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod variables;
+mod practice;
+
+fn main() {
+    // variables::main();
+    // variables::shadowing();
+    // practice::main();
+}
---
+++ src/variables.rs
@@ -0,0 +1,24 @@
+pub(crate) fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    let (a, mut b): (bool, bool) = (true, false);
+    println!("a = {:?}, b = {:?}", a, b);
+
+    b = true;
+    assert_eq!(a, b);
+}
+pub(crate) fn shadowing() {
+    let x = 5;
+    // 在main函数的作用域内对之前的x进行遮蔽
+    let x = x + 1;
+
+    {
+        // 在当前的花括号作用域内，对之前的x进行遮蔽
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+}
