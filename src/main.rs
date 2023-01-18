fe5d4b7b16ec5dd7127bd455c59fca80c1329fee
NanamiNakano <thynanami@foxmail.com>
2023-01-18 13:13:53 +0800

    hello world(late)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ mod variables;
 mod practice;
 
 fn main() {
+    helloworld();
     // variables::main();
     // variables::shadowing();
     // practice::main();
+}
+
+fn helloworld() {
+    println!("Hello, world!");
 }
